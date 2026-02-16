534ab43cf5d52fb5552b1d0fe8d25f87e338fce9
pankajyadav000227 <pankajyadav000227@gmail.com>
2026-02-17 00:40:13 +0530

    Fix elevenlabs import - remove extra .t

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify, send_file
-from elevenlabs.t import ElevenLabs
+from elevenlabs import ElevenLabs
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
